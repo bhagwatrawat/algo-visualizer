fe563c04582ed28d193066ac4f339d696d797b14
Bhagwat Rawat <bhagwat12rawat@gmail.com>
2022-09-03 14:42:08 +0530

    bubble sort

--- src/App.js
+++ src/App.js
@@ -2,88 +2,80 @@
  * For Tips and Advanced Usage read this Blog Post
  * https://levelup.gitconnected.com/integrating-p5-sketches-into-your-react-app-de44a8c74e91
  */
-import React, { useEffect, useRef, useState } from 'react';
-import Sketch from 'react-p5';
-import './App.css';
+import React, { useEffect, useRef, useState } from "react";
+import Sketch from "react-p5";
+import BubbleSort from './algorithm/bubblesort';
+import "./App.css";
 const App = () => {
-	let y = 0;
+  let y = 0;
   // const[arr,setArr]=useState([50,20,60,30,51,80])
-  let sort=null;
-  let swap=null;
-  let arr=[50,20,60,30,51,80];
-  let color=[[50,238,144],[50,238,144],[50,238,144],[50,238,144],[50,238,144],[50,238,144]]
+  let sort = null;
+  let swap = null;
+  let arr = [50, 20, 60, 30, 51, 80];
+  let color = [
+    [50, 238, 144],
+    [50, 238, 144],
+    [50, 238, 144],
+    [50, 238, 144],
+    [50, 238, 144],
+    [50, 238, 144],
+  ];
   const ref = useRef(null);
-  
-	const setup = (p5, parentRef) => {
-    console.log(parentRef)
-  let width=ref.current.clientWidth;
-  let height=ref.current.clientHeight;
-  
-		p5.createCanvas(width, height).parent(parentRef);
-	};
 
-	const draw = (p5) => {
-		p5.background(0);
-    for(let i=0;i<arr.length;i++){
+  const setup = (p5, parentRef) => {
+    console.log(parentRef);
+    let width = ref.current.clientWidth;
+    let height = ref.current.clientHeight;
+
+    p5.createCanvas(width, height).parent(parentRef);
+  };
+
+  const draw = (p5) => {
+    p5.background(0);
+    for (let i = 0; i < arr.length; i++) {
       // p5.fill(255, arr[i] * 1.3, 0);
 
-      p5.fill(color[i][0],color[i][1],color[i][2]);
-      p5.rect(i*20,p5.height-arr[i],20,arr[i]);
+      p5.fill(color[i][0], color[i][1], color[i][2]);
+      p5.rect(i * 20, p5.height - arr[i], 20, arr[i]);
     }
-		// p5.rect(y, 50, 50, 50); 
-  }
- 
-	const sortHandler=()=>{
-    
-    for(let i=0;i<arr.length-1;i++){
-      let x=i;
-       sort=setTimeout(function(){
-        for(let j=i+1;j<arr.length;j++){
-          if(arr[x]>arr[j]){
-            x=j;
+    // p5.rect(y, 50, 50, 50);
+  };
+
+  const sortHandler = () => {
+    for (let i = 0; i < arr.length - 1; i++) {
+      let x = i;
+      sort = setTimeout(function () {
+        for (let j = i + 1; j < arr.length; j++) {
+          if (arr[x] > arr[j]) {
+            x = j;
           }
         }
-        if(x===i){
-          color[x]=[60,15,60]
+        if (x === i) {
+          color[x] = [60, 15, 60];
+        } else {
+          color[i] = [0, 0, 255];
+          color[x] = [255, 0, 0];
         }
-        else{
-        color[i]=[0,0,255];
-        color[x]=[255,0,0];
-        }
-        console.log(i,x);
-         swap=setTimeout(function(){
-       
-          let temp=arr[i];
-          arr[i]=arr[x];
-          arr[x]=temp;
-         
-        color[i]=[50,238,144];
-      color[x]=[50,238,144];
-          
-        },300)
-       
-      
-      
-      },1000*i)
-      
+        console.log(i, x);
+        swap = setTimeout(function () {
+          let temp = arr[i];
+          arr[i] = arr[x];
+          arr[x] = temp;
 
+          color[i] = [50, 238, 144];
+          color[x] = [50, 238, 144];
+        }, 300);
+      }, 1000 * i);
     }
-  }
-  const resetHandler=()=>{
-    arr=[50,20,60,30,51,80];
-    clearTimeout(swap);
-    clearTimeout(sort);
-  }
-		return (
-			<div className="App">
-				<h1>react-p5</h1>
-        <div ref={ref} className="__App-canvas">
-				<Sketch setup={setup} draw={draw} />
-        </div>
-        <button onClick={sortHandler}>Sort</button>
-        <button onClick={resetHandler}>reset</button>
-			</div>
-		);
-	}
+  };
+  const resetHandler = () => {
+    arr = [50, 20, 60, 30, 51, 80];
+  };
+  return (
+    <div className="App">
+      <BubbleSort/>
+    </div>
+  );
+};
 
 export default App;
