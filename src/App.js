39c7123700378d9568601419e13ced90d3705d51
Bhagwat Rawat <bhagwat12rawat@gmail.com>
2022-09-04 14:34:51 +0530

    add range

--- src/App.js
+++ src/App.js
@@ -4,78 +4,16 @@
  */
 import React, { useEffect, useRef, useState } from "react";
 import Sketch from "react-p5";
-import BubbleSort from './algorithm/bubblesort';
+import Sorting from './algorithm/sortingAlgorithm/sorting.js';
 import "./App.css";
 const App = () => {
-  let y = 0;
-  // const[arr,setArr]=useState([50,20,60,30,51,80])
-  let sort = null;
-  let swap = null;
-  let arr = [50, 20, 60, 30, 51, 80];
-  let color = [
-    [50, 238, 144],
-    [50, 238, 144],
-    [50, 238, 144],
-    [50, 238, 144],
-    [50, 238, 144],
-    [50, 238, 144],
-  ];
-  const ref = useRef(null);
-
-  const setup = (p5, parentRef) => {
-    console.log(parentRef);
-    let width = ref.current.clientWidth;
-    let height = ref.current.clientHeight;
-
-    p5.createCanvas(width, height).parent(parentRef);
-  };
-
-  const draw = (p5) => {
-    p5.background(0);
-    for (let i = 0; i < arr.length; i++) {
-      // p5.fill(255, arr[i] * 1.3, 0);
-
-      p5.fill(color[i][0], color[i][1], color[i][2]);
-      p5.rect(i * 20, p5.height - arr[i], 20, arr[i]);
-    }
-    // p5.rect(y, 50, 50, 50);
-  };
-
-  const sortHandler = () => {
-    for (let i = 0; i < arr.length - 1; i++) {
-      let x = i;
-      sort = setTimeout(function () {
-        for (let j = i + 1; j < arr.length; j++) {
-          if (arr[x] > arr[j]) {
-            x = j;
-          }
-        }
-        if (x === i) {
-          color[x] = [60, 15, 60];
-        } else {
-          color[i] = [0, 0, 255];
-          color[x] = [255, 0, 0];
-        }
-        console.log(i, x);
-        swap = setTimeout(function () {
-          let temp = arr[i];
-          arr[i] = arr[x];
-          arr[x] = temp;
-
-          color[i] = [50, 238, 144];
-          color[x] = [50, 238, 144];
-        }, 300);
-      }, 1000 * i);
-    }
-  };
-  const resetHandler = () => {
-    arr = [50, 20, 60, 30, 51, 80];
-  };
+  
   return (
     <div className="App">
-      <BubbleSort/>
+      <Sorting/>
     </div>
   );
 };
 
 export default App;
+ 
